d0c73c533f67a2c7ca8eb41362bbed618cf7ff7e
Olexiy Zamkoviy <olexiy.z@gmail.com>
2009-11-30 19:44:44 +0200

    Added some front view stuff

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,16 @@ module ApplicationHelper
       link_to(caption + (count ? " (#{count})" : ''), item)
     end
   end
+
+  def albums_user_path(item)
+    return :controller => :index, :action => :albums, :id => item
+  end
+
+  def cat_user_path(item)
+    return :controller => :index, :action => :cat, :id => item
+  end
+
+  def photos_user_path(item)
+    return :controller => :index, :action => :images, :album_id => item
+  end
 end

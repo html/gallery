630ee2df924ab2df051714b44c75d95c91c1ef99
Olexiy Zamkoviy <olexiy.z@gmail.com>
2009-12-02 21:44:33 +0200

    Added attachment to album model

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,5 +1,6 @@
 class Album < ActiveRecord::Base
-  attr_accessible :title
+  attr_accessible :title, :image
   validates_presence_of :title
   has_many :photos
+  has_attached_file :image, :storage => :filesystem, :styles => { :thumb => ["100x100", :png]}, :whiny_thumbnails => true, :whiny => true
 end
--- test/unit/album_test.rb
+++ test/unit/album_test.rb
@@ -3,4 +3,5 @@ require 'test_helper'
 class AlbumTest < ActiveSupport::TestCase
   should_validate_presence_of :title
   should_have_many :photos
+  should_have_attached_file :image
 end

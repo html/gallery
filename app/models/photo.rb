337e61d44a6b0513191f6966701ff5ad4c403a25
Olexiy Zamkoviy <olexiy.z@gmail.com>
2009-12-05 11:13:18 +0200

    Added maximal dimmensions for photo attachment

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,9 @@
 class Photo < ActiveRecord::Base
   attr_accessible :photo
-  has_attached_file :photo, :storage => :filesystem, :styles => { :thumb => ["100x100", :png]}, :whiny_thumbnails => true, :whiny => true
+  has_attached_file :photo, :storage => :filesystem, :styles => {
+    :original => ["1024x768", :png],
+    :thumb => ["100x100", :png]
+  }, :whiny_thumbnails => true, :whiny => true
   validates_attachment_presence :photo
   belongs_to :album
   validates_associated :album

4d69578c78e4314cb30eac8af8ab9ded332f9880
Olexiy Zamkoviy <olexiy.z@gmail.com>
2010-01-21 00:13:01 +0200

    Added new menu

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,14 +1,23 @@
 class Category < ActiveRecord::Base
-  cattr_accessor :options
+  cattr_accessor :titles
   attr_accessible :title, :menu_id
   validates_presence_of :title
   validates_presence_of :menu_id
   validates_numericality_of :menu_id, :less_than_or_equal => 3, :more_than_or_equal => 1
   has_many :albums
+
   @@options = {
-    1 => 'WEDDINGS',
-    2 => 'PORTRAITS',
-    3 => 'TRAVEL'
+    1 => 'portfolio',
+    2 => 'vacancy',
+    3 => 'faq',
+    4 => 'na'
+  }
+
+  @@titles = {
+    1 => 'Weddings',
+    2 => 'Portraits',
+    3 => 'Travels',
+    4 => 'Nightlife'
   }
 
   def get_title

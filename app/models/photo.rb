c72a768b29eec9f27d51bd9e62a3d6f4db6b8eac
Olexiy Zamkoviy <olexiy.z@gmail.com>
2010-01-25 20:52:36 +0200

    Moving to jpg format instead of png

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,8 +2,9 @@ class Photo < ActiveRecord::Base
   attr_accessible :photo
   has_attached_file :photo, :storage => :filesystem, :styles => {
     :original => ["1024x768", :png],
-    :preview => ["550x315", :png],
-    :thumb => ["100x100#", :png]
+    :full => ["1024x768", :jpg],
+    :preview => ["550x315", :jpg],
+    :thumb => ["100x100#", :jpg]
   }, :whiny_thumbnails => true, :whiny => true
   validates_attachment_presence :photo
   belongs_to :album
